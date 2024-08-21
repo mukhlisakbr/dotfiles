5d58933e1f6cbca2e0361cbfb3ad214ad162bfcc
Mukhlis Akbarrudin <27577560+mukhlisakbr@users.noreply.github.com>
2024-08-21 23:49:49 +0700

    use catppuccin for everything

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      local gruvbox = require("gruvbox")
+      gruvbox.setup({
+        transparent_mode = true,
+      })
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
