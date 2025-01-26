2666e59114ee2e8834f5c456d4a685b1153f2632
Mukhlis Akbarrudin <27577560+mukhlisakbr@users.noreply.github.com>
2025-01-26 10:46:27 +0700

    update nvim colorscheme

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,29 +1,29 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = true,
-		opts = {
-			style = "night",
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
-			},
-		},
-	},
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		opts = {
-			styles = {
-				transparency = false,
-			},
-		},
-	},
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "rose-pine",
-		},
-	},
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
 }
