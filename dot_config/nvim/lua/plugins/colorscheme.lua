e0e8930f6ad7fef9cf2a240b6a4419c1f8d90e73
Mukhlis Akbarrudin <27577560+mukhlisakbr@users.noreply.github.com>
2025-08-13 21:48:38 +0700

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/colorscheme.lua Update .qwen/settings.json

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      style = "day",
+      style = "night",
       transparent = true,
       styles = {
         sidebars = "transparent",
