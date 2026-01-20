d8c062b2fc88ea2262c8f0a67d87857390b43321
Mukhlis Akbarrudin <27577560+mukhlisakbr@users.noreply.github.com>
2026-01-20 21:50:40 +0700

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,10 @@ return {
     opts = {
       style = "night",
       transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
   },
   {
